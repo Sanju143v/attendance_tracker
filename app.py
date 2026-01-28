f223e63d4b45517493bc54ed940aa3d52b7458a8
Sanju143v <svsanju341@gmail.com>
2026-01-28 15:51:05 +0530

    Fix Flask run config for Render

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,5 +7,4 @@ def hello_world():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True,host="http://127.0.0.1:5000/",port=5000)
-    
+    app.run(debug=True)
